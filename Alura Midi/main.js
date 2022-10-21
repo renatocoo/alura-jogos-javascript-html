7cc61056c5fa334d5be5db99bbe5303f27f138a3
Renatocoo <sdacostarenato@gmail.com>
2022-10-21 12:25:25 -0300

    finaliza aula 3

---
+++ Alura Midi/main.js
@@ -0,0 +1,14 @@
+function tocaSom(idElementoAudio){
+
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = function (){
+        tocaSom('#som_tecla_pom');
+    };
+    contador ++;
+}
