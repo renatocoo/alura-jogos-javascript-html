fcd048862dd9da32f4d09185dfe08d03adb96143
Renatocoo <sdacostarenato@gmail.com>
2022-10-21 13:48:24 -0300

    ajusta acessibilidade

--- Alura Midi/main.js
+++ Alura Midi/main.js
@@ -1,5 +1,11 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio).play();
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play()
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -12,5 +18,14 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
+
+    tecla.onkeydown = function (evento) {
+        if(evento.code === "Enter" || evento.code === "Space") {
+        tecla.classList.add('ativa');
+        }
+      }
+      tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+      }
 
 }               
