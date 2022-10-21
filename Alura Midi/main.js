7bcbbcc97205a618ecef1839b908ed64367306ca
Renatocoo <sdacostarenato@gmail.com>
2022-10-21 12:53:05 -0300

    conclui funcionamento midi

--- Alura Midi/main.js
+++ Alura Midi/main.js
@@ -1,14 +1,16 @@
-function tocaSom(idElementoAudio){
-
+function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
 
-while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = function (){
-        tocaSom('#som_tecla_pom');
-    };
-    contador ++;
-}
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}               
